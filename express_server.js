0d6354fd3604fef69601872b68f30303e6e923a6
Cheryet <C.heryet@outlook.com>
2022-11-01 10:46:45 -0600

    Fixed linking issue for edit link, completed edit function to edit/replace long URL

--- express_server.js
+++ express_server.js
@@ -47,6 +47,11 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:id', (req, res) => {
+  urlDatabase[req.params.id] = req.body.newLongURL
+  res.redirect('/urls')
+})
+
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect('/urls')
@@ -56,6 +61,7 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
