59be47f1870d58dc77e489830a36759bf46b4a9d
Cheryet <C.heryet@outlook.com>
2022-11-03 13:36:50 -0600

    esLinted file

--- test/helper_functions_test.js
+++ test/helper_functions_test.js
@@ -1,21 +1,21 @@
 const { assert } = require('chai');
 
-const { urlsForUser } = require('../helper_functions')
-const { shortURLExists } = require('../helper_functions')
-const { getIDFromEmail } = require('../helper_functions')
-const { correctPassword } = require('../helper_functions')
-const { emailExists } = require('../helper_functions')
-const { generateRandomString } = require('../helper_functions')
+const { urlsForUser } = require('../helper_functions');
+const { shortURLExists } = require('../helper_functions');
+const { getIDFromEmail } = require('../helper_functions');
+const { correctPassword } = require('../helper_functions');
+const { emailExists } = require('../helper_functions');
+const { generateRandomString } = require('../helper_functions');
 
 const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -33,37 +33,37 @@ const urlDatabase = {
 
 describe('getIDByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getIDFromEmail("user@example.com", users)
+    const user = getIDFromEmail("user@example.com", users);
     const expectedUserID = "userRandomID";
-    assert.equal(user, expectedUserID)
+    assert.equal(user, expectedUserID);
   });
 });
 
 describe('emailExists', function() {
   it('should return truthy', function() {
-    const email = 'user2@example.com'
+    const email = 'user2@example.com';
    
-    assert.isTrue(emailExists(email, users))
+    assert.isTrue(emailExists(email, users));
   });
 });
 
 describe('shortURLExists', function() {
   it('should return truthy', function() {
-    const shortURL = "9sm5xK"
+    const shortURL = "9sm5xK";
    
-    assert.isTrue(shortURLExists(shortURL, urlDatabase))
+    assert.isTrue(shortURLExists(shortURL, urlDatabase));
   });
 });
 
 describe('urlsForUser', function() {
   it('should return all short and long urls for UserID', function() {
-    const userID = "aJ48lW"
+    const userID = "aJ48lW";
     const results = {
       "b2xVn2" : "http://www.lighthouselabs.ca",
       "9sm5xK" : "http://www.google.com"
-  }
+    };
    
-    assert.deepEqual(urlsForUser(userID, urlDatabase), results)
+    assert.deepEqual(urlsForUser(userID, urlDatabase), results);
   });
 });
 
